b9eebb32a221ae40f61ba0f48feff99cfc1c37d9
NolaZheng <mania001988@gmail.com>
2020-05-15 12:43:24 +0800

    env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './config.env' })
 
 const app = require('./app')
 
-// console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV)
 
 ////////// START SERVER ///////////
 
